708a39296139abae74921af6527ad3d893f8a8cf
Fernando Souza <fernandoantoniocardosodesouza@gmail.com>
2025-08-15 15:44:11 -0300

    fix: component loading

--- apps/web/src/components/loading.tsx
+++ apps/web/src/components/loading.tsx
@@ -2,7 +2,7 @@ import { Logo } from "./logo";
 
 export const LoadingComponent = () => {
   return (
-    <div className="fixed flex-col gap-1 inset-0 z-50 flex items-center justify-center bg-white/70 backdrop-blur-sm">
+    <div className="fixed flex-col gap-1 inset-0 z-[999] flex items-center justify-center bg-white/70 backdrop-blur-sm">
       <Logo className="size-20 motion-preset-stretch " />
       <span className="animate-pulse">Carregando...</span>
     </div>
