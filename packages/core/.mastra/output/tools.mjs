f463510cea1d58d9a2e6ff148fc943d9c070a310
Fernando Souza <fernandoantoniocardosodesouza@gmail.com>
2025-08-23 15:27:27 -0300

    fix: vector

--- packages/core/.mastra/output/tools.mjs
+++
@@ -1,6 +0,0 @@
-import * as tool0 from './tools/9b43c1ca-ab33-4aa1-83e0-0b005e83509f.mjs';
-import * as tool1 from './tools/16fa4b83-59a0-4e31-9930-26ba2c8855b7.mjs';
-import * as tool2 from './tools/cee67780-756c-43e2-86c0-784c14029b20.mjs';
-import * as tool3 from './tools/44d7ab65-b1a4-43da-b12a-bf1778134d9f.mjs';
-        
-                export const tools = [tool0, tool1, tool2, tool3]
