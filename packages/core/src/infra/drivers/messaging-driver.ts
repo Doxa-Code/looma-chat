d917f61cbdf1173861937425c9d09157f60c3561
Fernando Souza <fernandoantoniocardosodesouza@gmail.com>
2025-09-01 13:16:36 -0300

    refactor: messaging driver

--- packages/core/src/infra/drivers/messaging-driver.ts
+++ packages/core/src/infra/drivers/messaging-driver.ts
@@ -15,7 +15,13 @@ interface MessagingDriver {
 
 export class SQSMessagingDriver implements MessagingDriver {
   async sendMessageToQueue(data: SendMessageToQueueProps): Promise<boolean> {
-    const sqsClient = new SQSClient({});
+    const sqsClient = new SQSClient({
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+      },
+      region: process.env.AWS_DEFAULT_REGION!,
+    });
 
     const command = new SendMessageCommand({
       QueueUrl: data.queueUrl,
