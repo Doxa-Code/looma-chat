fe01023b5198a624e93747aff9abb04414b14b75
Fernando Souza <fernandoantoniocardosodesouza@gmail.com>
2025-09-02 10:36:10 -0300

    fix: status

--- apps/web/src/components/badge-status.tsx
+++ apps/web/src/components/badge-status.tsx
@@ -1,32 +1,41 @@
-import { StatusValue } from "@looma/core/domain/value-objects/status";
+import { Status, StatusValue } from "@looma/core/domain/value-objects/status";
 import { Badge } from "./ui/badge";
 
 type Props = {
   status: StatusValue;
 };
 export const BadgeStatus: React.FC<Props> = (props) => {
+  const status = Status.create(props.status);
   if (props.status === "budget") {
-    return <Badge className="bg-amber-500">Em orçamento</Badge>;
+    return <Badge className="bg-amber-500">{status.formatted}</Badge>;
   }
 
   if (props.status === "cancelled") {
-    return <Badge className="bg-rose-500">Cancelado</Badge>;
+    return <Badge className="bg-rose-500">{status.formatted}</Badge>;
   }
 
   if (props.status === "expired") {
-    return <Badge className="bg-muted text-muted-foreground">Expirado</Badge>;
+    return (
+      <Badge className="bg-muted text-muted-foreground">
+        {status.formatted}
+      </Badge>
+    );
   }
 
   if (props.status === "finished") {
-    return <Badge className="bg-green-500">Finalizado</Badge>;
+    return <Badge className="bg-green-500">{status.formatted}</Badge>;
+  }
+
+  if (props.status === "processing") {
+    return <Badge className="bg-orange-500">{status.formatted}</Badge>;
   }
 
   if (props.status === "order") {
-    return <Badge className="bg-primary">Pedido realizado</Badge>;
+    return <Badge className="bg-primary">{status.formatted}</Badge>;
   }
 
   if (props.status === "shipped") {
-    return <Badge className="bg-teal-500">Entrega</Badge>;
+    return <Badge className="bg-teal-500">{status.formatted}</Badge>;
   }
 
   return <></>;
