8473915834309f5007a098b10198d04b488e0b9f
Fernando Souza <fernandoantoniocardosodesouza@gmail.com>
2025-09-16 15:23:31 -0300

    fix: envs

--- packages/core/src/infra/ai/config/llms/azure.ts
+++ packages/core/src/infra/ai/config/llms/azure.ts
@@ -7,7 +7,7 @@ export const azure = createAzure({
 });
 
 export const azureEmbeddings = createAzure({
-  apiKey: process.env.AZURE_API_KEY || "",
+  apiKey: process.env.AZURE_MEDIA_API_KEY || "",
   baseURL: process.env.AZURE_ENDPOINT || "",
   apiVersion: process.env.AZURE_API_VERSION || "",
   resourceName: "text-embedding-3-small",
